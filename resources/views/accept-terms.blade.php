af2e60a8c5c5fc6f870ef9936769951a150fd7e5
tarangdonga <tarangkdonga@gmail.com>
2025-11-05 04:10:36 -0500

    Accept terms page update

--- resources/views/accept-terms.blade.php
+++ resources/views/accept-terms.blade.php
@@ -12,13 +12,13 @@
     <div class="text-center mb-6 mt-8">
         <img src="{{ asset('assets/s_logo.png') }}" alt="Site Logo" class="mx-auto h-[20vh] w-auto mb-4">
         <p class="text-lg text-gray-800">
-            *DISCLAIMER: Information from this site is meant to only be used as reference.
-            <br>Always make sure to notify the restaurant and your server about your food allergies.
+            <strong>*DISCLAIMER: Information from this site is meant to only be used as reference.
+                <br>Always make sure to notify the restaurant and your server about your food allergies.</strong>
         </p>
     </div>
 
     <!-- Accept Terms Form Section -->
-    <div class="w-full max-w-sm p-6 bg-white rounded-lg shadow-md">
+    <div class="w-full max-w-sm p-6 bg-white rounded-lg shadow-md sm:w-[90%]">
         <h2 class="text-2xl font-semibold text-center mb-6">Accept <a target="_blank" href="/"
                 class="hover:underline text-blue-500">Terms of Use</a></h2>
         <form method="POST" action="/accept-terms" class="space-y-4">

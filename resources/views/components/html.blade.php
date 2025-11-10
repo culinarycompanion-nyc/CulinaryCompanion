7ec10dcde87b1c136e34836a8bb38c44f070a57f
tarangdonga <tarangkdonga@gmail.com>
2025-11-10 15:41:08 -0500

    Favicon

--- resources/views/components/html.blade.php
+++ resources/views/components/html.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport"
         content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" type="image/png" href="{{ asset("assets/favicon.png") }}">
     <title>Culinary Companion</title>
     @vite(['resources/css/app.css'])
     <!-- <script src="https://cdn.tailwindcss.com"></script> -->

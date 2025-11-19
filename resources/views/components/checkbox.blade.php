4347086642d4b55ca9f53cfe77c1d8c98169dc70
tarangdonga <tarangkdonga@gmail.com>
2025-11-19 11:17:45 -0500

    Update

--- resources/views/components/checkbox.blade.php
+++ resources/views/components/checkbox.blade.php
@@ -1,12 +1,12 @@
 @vite(['resources/css/main.css'])
 
-<div class="checkbox-wrapper col-span-2">
+<div class="checkbox-wrapper z-10 col-span-2">
     <label class="checkbox-wrapper">
         <input type="checkbox" class="checkbox-input" name="checkboxes[]" value="{{ $value }}" {{ in_array($value, $selected ?? []) ? 'checked' : '' }} />
-            <span class="checkbox-tile">
-                <span class="checkbox-icon">
-                </span>
-                    <span class="checkbox-label">{{ $label }}</span>
+        <span class="checkbox-tile">
+            <span class="checkbox-icon">
             </span>
+            <span class="checkbox-label">{{ $label }}</span>
+        </span>
     </label>
 </div>
